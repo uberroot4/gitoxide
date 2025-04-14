af2a356fa0ef6363d07c9729309a4c5bfd3ae4bc
Manuel Stöger <manuel.stoeger@inso-world.com>
2025-04-14 18:14:35 +0200

    Removed target_env = "p2" for WASI to ensure compatibility with p3

--- gix-sec/src/identity.rs
+++ gix-sec/src/identity.rs
@@ -18,14 +18,14 @@ pub fn is_path_owned_by_current_user(path: &Path) -> std::io::Result<bool> {
 }
 
 // Wasi doesn't have a concept of a user, so this is implicitly true.
-#[cfg(target_os = "wasi")]
+#[cfg(any(target_os = "wasi", target_family = "wasm"))]
 mod impl_ {
     pub fn is_path_owned_by_current_user(_path: &std::path::Path) -> std::io::Result<bool> {
         Ok(true)
     }
 }
 
-#[cfg(all(not(windows), not(target_os = "wasi")))]
+#[cfg(all(not(windows), not(target_os = "wasi"), not(target_family = "wasm")))]
 mod impl_ {
     use std::path::Path;
 
